812f9878feca9eacf63ea19cec66027c5cc43ded
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2021-11-14 08:02:21 +0300

    add weekDays array

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 let femaleAkanNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 let maleAkanNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
