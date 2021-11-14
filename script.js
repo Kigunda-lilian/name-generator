0fe275dae0b7a0e0c97b8a12d2c4a9274cc1fe95
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2021-11-14 10:09:47 +0300

    add AkanNameGenerate function

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 let femaleAkanNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 let maleAkanNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
 let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+var name = AkanNameGenerate => {
+    let name = document.getElementById('success').value;
+    let date = document.getElementById('date').value;
+    let gender = document.getElementById('gender').value;
+
+    let dayOfWeek = new Date(day).getDay()
+
+    if (gender == 'female') {
+        console.log();
+        (`You were born on a ${weekDays[dayOfWeek]} and your Akan name is ${femaleAkanNames[dayOfWeek]}`)
+    } else if (gender == 'male') {
+        console.log();
+        (`You were born on a ${weekDays[dayOfWeek]} and your Akan name is ${maleAkanNames[dayOfWeek]}`)
+    } else {
+        console.log('You have not selected anything');
+    }
+}
