07f54051ceb639b63c49b9354627c27bfb6fb69c
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2021-11-15 08:48:56 +0300

    add text in landing page

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 let femaleAkanNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 let maleAkanNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
 let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+document.getElementById('submit').addEventListener("click", function() {
+    AkanNameGenerate();
+})
 
-function AkanNameGenerate(event) {
-    event.preventDefault
+function AkanNameGenerate() {
+
     let name = document.getElementById('name').value;
     let date = document.getElementById('date').value;
     let gender = document.getElementById('gender').value;
 
-    let dayOfWeek = new Date(day).getDay()
+    let dayOfWeek = new Date(date).getDay()
 
-    if (gender == 'female') {
-        console.log();
-        (`You were born on a ${weekDays[dayOfWeek]} and your Akan name is ${femaleAkanNames[dayOfWeek]}`)
+    if (gender == '' || date == "") {
+        console.log("fill in your date birth and gender")
+    } else if (gender == 'female') {
+        console.log(`${name} ,you were born on a ${weekDays[dayOfWeek]} and your Akan name is ${femaleAkanNames[dayOfWeek]}`)
     } else if (gender == 'male') {
-        console.log();
-        (`You were born on a ${weekDays[dayOfWeek]} and your Akan name is ${maleAkanNames[dayOfWeek]}`)
+        console.log(`${name},you were born on a ${weekDays[dayOfWeek]} and your Akan name is ${maleAkanNames[dayOfWeek]}`)
     } else {
         console.log('You have not selected anything');
     }
