de318112bfcdb71eec0c5f7b02beefc8d2b3ac38
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2021-11-15 12:17:22 +0300

    set the display attribute of RESULTS_div

--- script.js
+++ script.js
@@ -10,16 +10,25 @@ function AkanNameGenerate() {
     let name = document.getElementById('name').value;
     let date = document.getElementById('date').value;
     let gender = document.getElementById('gender').value;
+    const RESULTS_div = document.getElementById('results');
+    RESULTS_div.setAttribute("style", "display:block")
 
     let dayOfWeek = new Date(date).getDay()
 
     if (gender == '' || date == "") {
         console.log("fill in your date birth and gender")
+        RESULTS_div.innerHTML = "<br><br><br>fill in your date birth and gender"
     } else if (gender == 'female') {
         console.log(`${name} ,you were born on a ${weekDays[dayOfWeek]} and your Akan name is ${femaleAkanNames[dayOfWeek]}`)
+        RESULTS_div.innerText = `${name} ,you were born on a ${weekDays[dayOfWeek]} and your Akan name is ${femaleAkanNames[dayOfWeek]}`
     } else if (gender == 'male') {
         console.log(`${name},you were born on a ${weekDays[dayOfWeek]} and your Akan name is ${maleAkanNames[dayOfWeek]}`)
+        RESULTS_div.innerText = `${name} ,you were born on a ${weekDays[dayOfWeek]} and your Akan name is ${maleAkanNames[dayOfWeek]}`
     } else {
         console.log('You have not selected anything');
+        RESULTS_div.innerText = 'You have not selected anything'
     }
+    setTimeout(function() {
+        RESULTS_div.setAttribute("style", "display:none")
+    }, 3000);
 }
